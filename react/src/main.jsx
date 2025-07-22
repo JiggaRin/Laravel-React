d61a275e7ec2a62a55a015ff635ed598dd32bbe0
Oleg <olegbackdev@gmail.com>
2025-07-22 19:16:18 +0300

    implement Guest Layout + Login and Signup pages.

--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
+import { ContextProvider } from './contexts/ContextProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+   <React.StrictMode>
+      <ContextProvider>
+         <RouterProvider router={router} />
+      </ContextProvider>
+   </React.StrictMode>
+);
--- react/src/views/Signup.jsx
+++ react/src/views/Signup.jsx
@@ -1,3 +1,29 @@
+import { Link } from 'react-router-dom';
+
 export default function Signup() {
-    return <div>Signup</div>;
+
+   const onSubmit = (event) => {
+      event.preventDefault();
+   };
+
+    return (
+       <div className="login-signup-form animated fadeInDown">
+          <div className="form">
+             <form onSubmit={onSubmit}>
+                <h1 className="title">
+                   Signup for free
+                </h1>
+                <input type="text" placeholder="Full Name" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <input type="password" placeholder="Password Confirmation" />
+                <button className="btn btn-block">Signup</button>
+                <p className="message">
+                   Already registered?
+                   <Link to="/login">&nbsp; Sign in</Link>
+                </p>
+             </form>
+          </div>
+       </div>
+    );
 }
